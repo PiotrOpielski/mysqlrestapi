8c8cd8820a1c07b4391e45d7d7dbe91fc1e38591
Piotr Opielski <opielski.piotr@gmail.com>
2017-07-16 22:07:07 +0200

    spring initializer code upload

---
+++ src/main/java/com/mysqlapi/MysqlapiApplication.java
@@ -0,0 +1,12 @@
+package com.mysqlapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlapiApplication.class, args);
+	}
+}
