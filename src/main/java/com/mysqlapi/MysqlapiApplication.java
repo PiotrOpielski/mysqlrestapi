879e54be8b4c320d84d79e8420cd34e996fa003a
Piotr Opielski <opielski.piotr@gmail.com>
2017-07-18 11:27:56 +0200

    code reformat

--- src/main/java/com/mysqlapi/MysqlapiApplication.java
+++ src/main/java/com/mysqlapi/MysqlapiApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MysqlapiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MysqlapiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MysqlapiApplication.class, args);
+    }
 }
