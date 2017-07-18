e5733199905b807760817bcf5244298310474341
Piotr Opielski <opielski.piotr@gmail.com>
2017-07-18 11:30:14 +0200

    learning of using PathVariable with a mysql

--- src/main/java/com/mysqlapi/Controller.java
+++ src/main/java/com/mysqlapi/Controller.java
@@ -1,15 +1,33 @@
 package com.mysqlapi;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+import java.util.Map;
 
 
 @RestController
-@RequestMapping("/")
 public class Controller {
-    @RequestMapping("")
-    public String homepageController(){
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+
+    @RequestMapping("/")
+    public String homepageController() {
+
         return "MySQL REST api homepage.";
     }
+
+    @RequestMapping(value = "testTable/{table}", method = RequestMethod.GET)
+    public List<Map<String, Object>> selectAllFromTable(@PathVariable String table) {
+
+        return jdbcTemplate.queryForList("SELECT * FROM testTable WHERE id = ? ;", new Object[]{table});
+
+    }
+
 
 }
