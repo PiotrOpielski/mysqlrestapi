2bcc9e2ffb0c9b403b377a04c4bb65b366e9e3f8
Piotr Opielski <opielski.piotr@gmail.com>
2017-07-24 22:05:26 +0200

    Improve testing (still learning)

--- src/test/java/com/mysqlapi/StatementControllerTests.java
+++ src/test/java/com/mysqlapi/StatementControllerTests.java
@@ -2,23 +2,29 @@ package com.mysqlapi;
 
 
 import org.junit.Test;
+
+import java.io.IOException;
+
 import static org.junit.Assert.assertEquals;
 
 public class StatementControllerTests {
 
-    StatementsController statementsController = new StatementsController();
-    String statements =  null;
+    StatementsController statementsController = null;
+    String statements = null;
 
+    public StatementControllerTests() throws IOException {
+        statementsController = new StatementsController("src/test/resources/statements.json");
+    }
 
 
     @Test
     public void testGetFileAsString() throws Exception {
-        assertEquals(statementsController.getFileAsString("src/test/resources/statements.json"), "[{\"id\":1,\"fieldInt\":5,\"fieldString\":\"test\"}]");
+        assertEquals(statementsController.getJSONFileString(), "[{\"id\":1,\"fieldInt\":5,\"fieldString\":\"test\"}]");
     }
+
     @Test
-    public void testGetStringAsJSONObject() throws Exception{
-        statements = statementsController.getFileAsString("src/test/resources/statements.json");
-        assertEquals(statementsController.getStringAsJSONArray(statements).getJSONObject(0).getString("fieldString"),"test");
+    public void testGetStringAsJSONObject() throws Exception {
+        assertEquals(statementsController.getJSONFileArray().getJSONObject(0).getString("fieldString"), "test");
 
     }
 
