237ec78ca91addb933265bd9ab9b4046a27350ea
Piotr Opielski <opielski.piotr@gmail.com>
2017-07-24 22:04:38 +0200

    Add getters for variables

--- src/main/java/com/mysqlapi/StatementsController.java
+++ src/main/java/com/mysqlapi/StatementsController.java
@@ -1,19 +1,34 @@
 package com.mysqlapi;
 
-//zrobić konfigurację w pliku java
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.springframework.stereotype.Component;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 @Component
 public class StatementsController {
 
-    public String getFileAsString(String filename) throws FileNotFoundException, IOException{
+    private String JSONFileString = null;
+    private JSONArray JSONFileArray = null;
+
+    public StatementsController(String filename) throws IOException, JSONException {
+        JSONFileString=getFileAsString(filename);
+        JSONFileArray = getStringAsJSONArray(JSONFileString);
+    }
+
+    public String getJSONFileString(){
+        return JSONFileString;
+    }
+
+    public JSONArray getJSONFileArray(){
+        return JSONFileArray;
+    }
+
+    public String getFileAsString(String filename) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader(filename));
         StringBuilder sb = new StringBuilder();
         String line = br.readLine();
@@ -21,7 +36,7 @@ public class StatementsController {
             sb.append(line);
             line = br.readLine();
         }
-       return sb.toString();
+        return sb.toString();
     }
 
     public JSONArray getStringAsJSONArray(String string) throws JSONException {
