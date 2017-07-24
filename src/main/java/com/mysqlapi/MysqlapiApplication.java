311f4d5e0047c385f722ea828944ecb9268c19f8
Piotr Opielski <opielski.piotr@gmail.com>
2017-07-24 22:03:48 +0200

    Add configuration for statementsController bean

--- src/main/java/com/mysqlapi/MysqlapiApplication.java
+++ src/main/java/com/mysqlapi/MysqlapiApplication.java
@@ -2,11 +2,19 @@ package com.mysqlapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class MysqlapiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MysqlapiApplication.class, args);
+    }
+
+    @Bean
+    public StatementsController statementsController() throws IOException {
+        return new StatementsController("src/main/resources/statements.json");
     }
 }
