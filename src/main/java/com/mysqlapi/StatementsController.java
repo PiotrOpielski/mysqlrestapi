4f388de4abe6c3d91b9539eae2afcb316c066c59
Piotr Opielski <opielski.piotr@gmail.com>
2017-07-25 12:01:53 +0200

    Add methods to parse statements from statements.json

--- src/main/java/com/mysqlapi/StatementsController.java
+++ src/main/java/com/mysqlapi/StatementsController.java
@@ -3,6 +3,7 @@ package com.mysqlapi;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.stereotype.Component;
 
 import java.io.BufferedReader;
@@ -16,16 +17,34 @@ public class StatementsController {
     private JSONArray JSONFileArray = null;
 
     public StatementsController(String filename) throws IOException, JSONException {
-        JSONFileString=getFileAsString(filename);
+        JSONFileString = getFileAsString(filename);
         JSONFileArray = getStringAsJSONArray(JSONFileString);
     }
 
-    public String getJSONFileString(){
+    public String getJSONFileString() {
         return JSONFileString;
     }
 
-    public JSONArray getJSONFileArray(){
+    public JSONArray getJSONFileArray() {
         return JSONFileArray;
+    }
+
+    public JSONObject getJSONObjectFromArray(int x) {
+        return JSONFileArray.getJSONObject(x);
+    }
+
+
+    public String getStatementFromId(String id) {
+        for(int x=0;x<getJSONObjectsNumberInJSONArray();x++){
+            if(getJSONObjectFromArray(x).getString("id").equals(id)){
+                return getJSONObjectFromArray(x).getString("query");
+            }
+        }
+        return null;
+    }
+
+    private int getJSONObjectsNumberInJSONArray(){
+        return JSONFileArray.length();
     }
 
     private String getFileAsString(String filename) throws IOException {
