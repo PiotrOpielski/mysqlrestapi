4db7f2a5809b7876ea6116de5b6205b40ee4069f
Piotr Opielski <opielski.piotr@gmail.com>
2017-07-25 16:09:16 +0200

    Add AuthProvider

---
+++ src/main/java/com/mysqlapi/security/AuthProvider.java
@@ -0,0 +1,33 @@
+package com.mysqlapi.security;
+
+
+import com.mysqlapi.exception.AuthException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthProvider implements AuthenticationProvider {
+
+    @Autowired
+    private UserAuthenticationService authenticationService;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        User user = null;
+        try {
+            user = authenticationService.authenticateToken((String) authentication.getCredentials());
+        } catch (AuthException e) {
+            e.printStackTrace();
+        }
+        return new AuthUserToken(user.getUsername(), user.getUserRoles());
+    }
+
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return JwtAuthToken.class.equals(aClass);
+    }
+}
